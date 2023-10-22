54b47184ea936184c61b32413c8f72ae666cbd11
satyakotari <148609331+satyakotari@users.noreply.github.com>
2023-10-22 11:22:31 +0530

    Delete src directory

--- src/basicprograms/Helloworld.java
+++
@@ -1,8 +0,0 @@
-package basicprograms;
-public class Helloworld 
-{
-	public static void main(String[] args)
-{
-		System.out.println("hello eclipse application");
-}
-}
--- src/www/Git/Test/GitSecondTest.java
+++
@@ -1,7 +0,0 @@
-package www.Git.Test;
-
-public class GitSecondTest {
-  public static void main(String[] args) {
-	System.out.println("git second file");
-}
-	}
